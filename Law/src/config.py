8479e3df5e51e9295fef8e7cd2f32c25fcd5808a
Sakuraba Ema <3265156700@qq.com>
2025-11-05 11:27:19 +0800

    Update config.py

--- Law/src/config.py
+++ Law/src/config.py
@@ -1,25 +1,28 @@
 import torch
 
 class Config:
-    data_path = "../data/DISC-Law-SFT-Pair-QA-released.jsonl"
-    triplet_data_path = "../data/DISC-Law-Triplet-QA-released.jsonl"
-    lora_data_path = "../data/lora_data.jsonl"
+    # ====== 数据路径 ======
+    data_path = "../data/DISC-Law-SFT-Pair-QA-released.jsonl"  # 训练数据路径
+    triplet_data_path = "../data/DISC-Law-Triplet-QA-released.jsonl" # 三元组数据路径
+    lora_data_path = "../data/lora_data.jsonl" # lora数据路径
 
-    baichuan_model_path = "../models/baichuan2-7b-base/"
-    bge_large_zh_model_path = "../models/BAAI/bge-large-zh"
-    bge_rerank_model_path = "../models/BAAI/bge-reranker-zh"
-    dtype = torch.float16
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # ===== 模型路径与配置 ======
+    baichuan_model_path = "../models/baichuan2-7b-base/" # Base 模型路径
+    bge_large_zh_model_path = "../models/BAAI/bge-large-zh" # BGE Large ZH 模型路径,用与向量编码
+    bge_rerank_model_path = "../models/BAAI/bge-reranker-zh" # BGE Reranker 模型路径
+    dtype = torch.float16 # 数据类型
+    device = "cuda" if torch.cuda.is_available() else "cpu" # 设备
+    tokenizer_max_length = 1024 # tokenizer 最大长度
 
-    tokenizer_max_length = 1024
-
-    lora_r = 8 # lora rank
+    # ===== LoRA 配置 ======
+    lora_r = 8 # lora r
     lora_alpha = 32 # lora alpha
     lora_dropout = 0.1 # lora dropout
     lora_target_modules = ["o_proj", "gate_proj", "down_proj"] # 适配器模块
 
+    # ===== 训练参数 ======
     apapter_output_dir = "../models/lora_legal_qa_adapter" # 保存适配器的目录路径
-    new_apapter_output_dir = "../models/lora_new_legal_qa_adapter"
+    new_apapter_output_dir = "../models/lora_new_legal_qa_adapter" # 使用三元组数据训练后的适配器路径
     overwrite_output_dir = True, # 覆盖输出目录中的内容
     num_train_epochs = 3 # 训练轮数
     per_device_train_batch_size = 4 # 每个设备的训练批次大小
@@ -35,28 +38,28 @@ class Config:
     warmup_steps = 100 # 学习率预热步数
     max_grad_norm = 1.0 # 梯度裁剪的最大范数
 
-    # pipeline
-    task = "text-generation"
-    max_new_tokens = 512
-    do_sample=True
-    temperature=0.7
-    top_p=0.9
-    repetition_penalty=1.1
+    # ===== Pipeline ======
+    task = "text-generation" # 任务名称
+    max_new_tokens = 512 # 最大生成长度
+    do_sample=True # 是否使用采样
+    temperature=0.7 # 采样温度
+    top_p=0.9 # 采样 nucleus
+    repetition_penalty=1.1 # 重复惩罚
 
-    # 新增的产物路径
-    faiss_index_dir = "../index/faiss_legal_qa_index"
-    tokenized_corpus_path = "../index/tokenized_corpus.json"
-    chunks_corpus_path = "../index/chunks_corpus.jsonl"
-    chunk_size = 1024
-    chunk_overlap = 128
+    # ===== FAISS  ======
+    faiss_index_dir = "../index/faiss_legal_qa_index" # FAISS 索引目录
+    tokenized_corpus_path = "../index/tokenized_corpus.json" # tokenized_corpus.json
+    chunks_corpus_path = "../index/chunks_corpus.jsonl" # chunks_corpus.jsonl
+    chunk_size = 1024 # 分块大小
+    chunk_overlap = 128 # 分块重叠长度
   
-    # 检索超参数
+    # ===== 检索超参数 ======
     topk_simple = 5      # v1: 标准 RAG 检索 5 条
     topk_bm25 = 50       # v2: BM25 召回
     topk_emb = 50        # v2: Embedding 召回
     topk_combined = 50   # v2: RRF 融合后
     topk_rerank = 5      # v2: 最终 Rerank 后取 5 条
-    rrf_k = 60
+    rrf_k = 60           # RRF 融合参数
 
     # 评测集大小
     rag_evaluation_sample_size = 100 # 从语料库中随机抽取200条进行评测
